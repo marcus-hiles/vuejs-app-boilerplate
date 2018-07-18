88c0de8dce1343c0ee590fdb3ea5b7384030142e
Marcus Hiles <39972880+marcus-hiles@users.noreply.github.com>
2018-07-18 16:00:54 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Dashboard
   }
 }
 </script>
