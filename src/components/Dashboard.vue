ea082a7d7e7a861a5e23740be05852cac022e79f
Marcus Hiles <39972880+marcus-hiles@users.noreply.github.com>
2018-10-19 09:53:51 -0500

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -19,13 +19,6 @@
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
     </ul>
     <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
